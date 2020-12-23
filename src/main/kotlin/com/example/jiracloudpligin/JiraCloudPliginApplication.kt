ef1ede3f50bfe3a54c18e85e46c74b8132dfd30d
Nikosar <randomregistr@gmail.com>
2020-12-23 11:18:42 +0300

    initial commit

---
+++ src/main/kotlin/com/example/jiracloudpligin/JiraCloudPliginApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jiracloudpligin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JiraCloudPliginApplication
+
+fun main(args: Array<String>) {
+    runApplication<JiraCloudPliginApplication>(*args)
+}
