e0bdf0cfaf255a7b1d2c51f5138ec729b7c4f601
Nikosar <randomregistr@gmail.com>
2020-12-29 00:13:09 +0300

    projects and users pickers WIP

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,44 +1,64 @@
-const MAX_RESULTS = 50;
+const MAX_RESULTS = 10;
+
+function paginatedQuery(url, query, page) {
+    return AP.request({
+        url: `${url}?query=${query}&maxResults=${MAX_RESULTS}&startAt=${(MAX_RESULTS * (page - 1))}`,
+        type: "GET",
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+}
+
+function projectsById(url, id) {
+    return AP.request({
+        url: `${url}/${id}`,
+        type: "GET",
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+}
 
 $(document).ready(async function () {
     const projectSelect = AJS.$("#project-select").auiSelect2({
-        dataType: "json",
+        multiple: true,
         ajax: {
+            url: "/rest/api/3/project/search",
             data: function (params, page) {
+                console.log(params);
                 return {
-                    search: params,
+                    query: params,
                     page: page || 1
                 }
             },
             transport: function (params) {
-                return getProjects(params.data.search, params.data.page, MAX_RESULTS)
-                    .then((resp) => params.success(JSON.parse(resp.body))
-                        .catch((e) => params.failure(e)));
-
+                paginatedQuery(params.url, params.data.query, params.data.page)
+                    .then((data) => params.success(JSON.parse(data.body)))
+                    .catch(e => params.error(e))
             },
             results: function (resp) {
-                console.log(resp)
                 let mappedResult = []
                 Object.values(resp.values).forEach((project) => {
                     mappedResult.push({id: project.id, text: project.name});
                 })
-                console.log(mappedResult)
                 return {
                     results: mappedResult,
                     pagination: {
-                        more: false
-                    }//(resp.startAt + resp.maxResults) <= resp.total
+                        more: (resp.startAt + resp.maxResults) <= resp.total
+                    }
                 }
             }
         }
     });
-    // let projects = getProjects(projectSelect);
     const userSelect = AJS.$("#user-select").auiSelect2();
     let users = getUsers(userSelect, "db");
-    // await projects;
     await users;
 
-    getSettings();
+    projectSelect.on("select2-selecting", function () {
+        console.log("selectsadasdasdasd");
+    })
+    fetchAndSetSettings(projectSelect);
 
     AJS.$("#save-settings").on("click", function () {
         const button = this;
@@ -50,17 +70,10 @@ $(document).ready(async function () {
     })
 })
 
-
-async function getProjects(search, page, maxResults) {
-    const offset = maxResults * (page - 1)
-    return AP.request({
-        url: `/rest/api/3/project/search?query=${search}&startAt=${offset}`,
-        type: "GET",
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-}
+//
+// async function getProjects(search, page, maxResults, url) {
+//     return
+// }
 
 function createOptionIfNotExist(name, id, select2) {
     let hasOption = select2.find("option[value='" + id + "']").length;
@@ -85,19 +98,68 @@ async function getUsers(select2, search) {
     }).catch(e => console.log(e))
 }
 
-async function getSettings() {
+async function fetchAndSetSettings(projectSelect) {
     return fetch("/settings", {
         "headers": {
             "Authorization": await token()
         }
     }).then(resp => resp.json())
-        .then(json => setSettings(json));
+        .then(json => setSettings(json, projectSelect));
 }
 
-function setSettings(settings) {
+function setSettings(settings, projectSelect) {
     $("#name").val(settings.name)
     $("#flag").prop("checked", settings.flag)
-    $("#project-select").val(settings.projects).trigger("change")
+    settings.projects.forEach(project => {
+        projectsById("/rest/api/3/project", project)
+            .then(data => JSON.parse(data.body))
+            .then(project => ({text: project.name, id: project.id}))
+            .then(project => {
+                const option = new Option(project.name, project.id, true, true);
+                projectSelect.append(option).trigger("change");
+
+                console.log("init trigger")
+                projectSelect.trigger({
+                    type: "select2-selecting",
+                    params: {
+                        data: project
+                    }
+                })
+            })
+
+            // .then((json) => console.log(json))
+
+//         // Set up the Select2 control
+//         $('#mySelect2').select2({
+//             ajax: {
+//                 url: '/api/students'
+//             }
+//         });
+//
+// // Fetch the preselected item, and add to the control
+//         var studentSelect = $('#mySelect2');
+//         $.ajax({
+//             type: 'GET',
+//             url: '/api/students/s/' + studentId
+//         }).then(function (data) {
+//             // create the option and append to Select2
+//             var option = new Option(data.full_name, data.id, true, true);
+//             studentSelect.append(option).trigger('change');
+//
+//             // manually trigger the `select2:select` event
+//             studentSelect.trigger({
+//                 type: 'select2:select',
+//                 params: {
+//                     data: data
+//                 }
+//             });
+//         });
+
+        //TODO check that project exists?
+        createOptionIfNotExist(project.name, project.id, projectSelect)
+    })
+
+    // $("#project-select").val(settings.projects).trigger("change")
     $("#user-select").val(settings.users).trigger("change")
 }
 
